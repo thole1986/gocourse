9f8050592b6af79239d3d1f0e3e5dfda45bff457
ThoLe <tholhlearning@gmail.com>
2025-06-19 18:50:39 +0700

    Finish pointers

---
+++ pointers.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var ptr *int
+	var a int = 10
+
+	// Pointer to memory addr of a
+	ptr = &a // referencing
+
+	fmt.Println(a)
+	fmt.Println(ptr)
+	// Get memory addr of viriable
+	// fmt.Println(ptr)
+
+	// Using * to get actual value
+	// fmt.Println(*ptr) // dereferencing a point
+
+	// if ptr == nil {
+	// 	fmt.Println("Pointer is nill")
+	// }
+
+	modifyValue(ptr)
+	fmt.Println(a)
+}
+
+// Add param to function accept a pointer
+func modifyValue(ptr *int) {
+	// Dereferencing ptr or get actual value
+	// of ptr param
+	*ptr++
+}
