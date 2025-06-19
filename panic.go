d7195772807536bfb3bdf7d20436b42be35de74c
ThoLe <tholhlearning@gmail.com>
2025-06-19 09:20:04 +0700

    Finish panic lession

---
+++ panic.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	// panic(interface{})
+
+	// Example of a valid input
+	process(10)
+
+	// Example of a invalid input
+	process(-3)
+}
+
+func process(input int) {
+
+	defer fmt.Println("Deferred 1")
+	defer fmt.Println("Deferred 2")
+
+	if input < 0 {
+		fmt.Println("Before Panic")
+		// Panic run the last after defer functions
+		panic("Input  must be a non-negative number")
+
+		// fmt.Println("After Panic")
+		// defer ftm.Println("Deferred 3")
+	}
+	fmt.Println("Processing input: ", input)
+}
