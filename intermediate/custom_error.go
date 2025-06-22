f9f1451c73ca28647d1f2f624a5a120d739bb452
ThoLe <tholhlearning@gmail.com>
2025-06-22 21:24:02 +0700

    Working on lession string functions

---
+++ intermediate/custom_error.go
@@ -0,0 +1,53 @@
+package intermediate
+
+import (
+	"errors"
+	"fmt"
+)
+
+func main() {
+
+	err := doSomething()
+
+	if err != nil {
+		fmt.Print(err)
+		return
+	}
+
+}
+
+type customError struct {
+	code    int
+	message string
+	er      error
+}
+
+// Error returns the error message. Implementing Error() method
+// error of interface
+func (e *customError) Error() string {
+	return fmt.Sprintf("Error %d: %s", e.code, e.message)
+}
+
+// Function that return a custom error
+// func doSomething() error {
+// 	return &customError{
+// 		code:    500,
+// 		message: "Something went wrong!",
+// 	}
+// }
+
+func doSomething() error {
+	err := doSomethingElse()
+	if err != nil {
+		return &customError{
+			code:    500,
+			message: "Something went wrong!",
+			er:      err,
+		}
+	}
+	return nil
+}
+
+func doSomethingElse() error {
+	return errors.New("internal error")
+}
