97bcf7057d7db7ab74f59795d61505d6bcf0df19
ThoLe <tholhlearning@gmail.com>
2025-06-24 13:08:38 +0700

    Starting lession times.go

--- regular_expressions.go
+++ regular_expressions.go
@@ -10,5 +10,45 @@ func main() {
 	fmt.Println(`He said, "I am great"`)
 
 	// Compile a regex pattern to match email address.
-	re := regexp.MustCompile()
+	re := regexp.MustCompile(`[a-zA-Z0-9._+%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}`)
+
+	// Test strings.
+	email1 := "user@email.com"
+	email2 := "invalid_email"
+
+	// Match
+	fmt.Println("Email1: ", re.MatchString(email1))
+	fmt.Println("Email2: ", re.MatchString(email2))
+
+	// Capturing Groups
+	// Compile a regex pattern to capture date components.
+	re = regexp.MustCompile(`(\d{4})-(\d{2})-(\d{2})`)
+
+	//Test string
+	date := "2024-07-30"
+	// Find all submatches
+	submatches := re.FindStringSubmatch(date)
+	fmt.Println(submatches)
+	fmt.Println(submatches[0]) // Extract full
+	fmt.Println(submatches[1]) // Extract year
+	fmt.Println(submatches[2]) // Extract month
+	fmt.Println(submatches[3]) // Extract day
+
+	// Source string
+	str := "Hello World"
+
+	re = regexp.MustCompile(`[aeiou]`)
+	result := re.ReplaceAllString(str, "*")
+	fmt.Println(result)
+
+	// i -case insensitive
+	// m - mullti line model
+	// s - dot matches all
+
+	re = regexp.MustCompile(`(?i)go`)
+
+	// Test string
+	text := "Golang is great"
+	// Match
+	fmt.Println("Match: ", re.MatchString(text))
 }
