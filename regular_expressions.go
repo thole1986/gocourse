4d7121b3a9103e4fb9e9405d370951dea2682d8e
ThoLe <tholhlearning@gmail.com>
2025-06-24 11:26:57 +0700

    Continu

--- regular_expressions.go
+++ regular_expressions.go
@@ -1,3 +1,14 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"regexp"
+)
+
+func main() {
+	fmt.Println("He said, \n\"I am great\"")
+	fmt.Println(`He said, "I am great"`)
+
+	// Compile a regex pattern to match email address.
+	re := regexp.MustCompile()
+}
